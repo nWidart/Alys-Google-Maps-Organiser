4d6e1a14819190f378194869e6bbe29559741d1f
Nicolas Widart <n.widart@gmail.com>
2012-11-02 19:22:51 +0100

    production setup

--- paths.php
+++ paths.php
@@ -24,7 +24,7 @@
 $environments = array(
 
 	'local' => array('http://gmaps-org*', '*.dev'),
-	'development' => array('http://alys-gmaps.pagodabox.com')
+	'production' => array('http://alys-gmaps.pagodabox.com')
 
 );
 
